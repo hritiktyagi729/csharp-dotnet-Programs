0e8dbbd9f4dc802239fe23b7a6702cc8bdae0374
Hritik Tyagi <151499603+hritiktyagi729@users.noreply.github.com>
2024-08-08 11:50:19 +0530

    Add files via upload

---
+++ csharpPrograms/recrusivefilevalidationandchecker/Form2.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace t8
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.nasdatapath = new System.Windows.Forms.Button();
+            this.txt_source = new System.Windows.Forms.RichTextBox();
+            this.browsesource = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.richTextBox2, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.nasdatapath, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.txt_source, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.browsesource, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(56, 32);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(606, 302);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(3, 228);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(600, 71);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // nasdatapath
+            // 
+            this.nasdatapath.Location = new System.Drawing.Point(3, 153);
+            this.nasdatapath.Name = "nasdatapath";
+            this.nasdatapath.Size = new System.Drawing.Size(600, 69);
+            this.nasdatapath.TabIndex = 1;
+            this.nasdatapath.Text = "Main Nasa Data PAth";
+            this.nasdatapath.UseVisualStyleBackColor = true;
+            this.nasdatapath.Click += new System.EventHandler(this.Nasdatapath_Click);
+            // 
+            // txt_source
+            // 
+            this.txt_source.Location = new System.Drawing.Point(3, 78);
+            this.txt_source.Name = "txt_source";
+            this.txt_source.Size = new System.Drawing.Size(600, 69);
+            this.txt_source.TabIndex = 2;
+            this.txt_source.Text = "";
+            // 
+            // browsesource
+            // 
+            this.browsesource.Location = new System.Drawing.Point(3, 3);
+            this.browsesource.Name = "browsesource";
+            this.browsesource.Size = new System.Drawing.Size(600, 69);
+            this.browsesource.TabIndex = 0;
+            this.browsesource.Text = "Browse Source Recycle Folder";
+            this.browsesource.UseVisualStyleBackColor = true;
+            this.browsesource.Click += new System.EventHandler(this.Browsesource_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(332, 359);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(111, 75);
+            this.start.TabIndex = 4;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 563);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button nasdatapath;
+        private System.Windows.Forms.RichTextBox txt_source;
+        private System.Windows.Forms.Button browsesource;
+        private System.Windows.Forms.Button start;
+    }
+}
---
+++ csharpPrograms/recrusivefilevalidationandchecker/Form2.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+
+namespace t8
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Browsesource_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+                    txt_source.Text = selectedPath;
+                    //  MessageBox.Show("Selected Folder: " + selectedPath);
+                }
+            }
+        }
+
+        private void Nasdatapath_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+                    richTextBox2.Text = selectedPath;
+                    //   MessageBox.Show("Selected Folder: " + selectedPath);
+                }
+            }
+        }
+
+
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            string firstFolderPath = txt_source.Text;                                           //@"C:\path\to\first\folder";  // Change this to your first folder path
+            string secondFolderPath = richTextBox2.Text;                                      //  @"C:\path\to\second\folder"; // Change this to your second folder path
+            string csvFilePath = @"D:\temp.csv";                             // Change this to your output CSV file path
+
+            // Collect information from the first folder
+            Dictionary<string, (long Size, int FileCount, int DirectoryCount)> firstFolderInfo = new Dictionary<string, (long, int, int)>();
+            TraverseFolder(firstFolderPath, firstFolderInfo, firstFolderPath);
+
+            using (StreamWriter writer = new StreamWriter(csvFilePath))
+            {
+
+                // first line of the csv file
+                writer.WriteLine("RelativePath,FirstFolderSize,FirstFileCount,FirstDirectoryCount,SecondFolderSize,SecondFileCount,SecondDirectoryCount");
+
+                // Compare with the second folder
+                foreach (var kvp in firstFolderInfo)
+                {
+                    string relativePath = kvp.Key; // Already stored as relative path
+                    string secondFolderPathEquivalent = Path.Combine(secondFolderPath, relativePath);
+
+                    if (Directory.Exists(secondFolderPathEquivalent))
+                    {
+                        long secondFolderSize = GetDirectorySize(new DirectoryInfo(secondFolderPathEquivalent));
+                        int secondFileCount = Directory.GetFiles(secondFolderPathEquivalent).Length;
+                        int secondDirectoryCount = Directory.GetDirectories(secondFolderPathEquivalent).Length;
+
+                        writer.WriteLine($"{relativePath},{kvp.Value.Size},{kvp.Value.FileCount},{kvp.Value.DirectoryCount},{secondFolderSize},{secondFileCount},{secondDirectoryCount}");
+                    }
+                    else
+                    {
+                        writer.WriteLine($"{relativePath},{kvp.Value.Size},{kvp.Value.FileCount},{kvp.Value.DirectoryCount},0,0,0");
+                    }
+                }
+            }
+            MessageBox.Show("Done");
+        }
+
+
+        static void TraverseFolder(string folderPath, Dictionary<string, (long, int, int)> folderInfo, string rootFolderPath)
+        {
+            long folderSize = GetDirectorySize(new DirectoryInfo(folderPath));
+            int fileCount = Directory.GetFiles(folderPath).Length;
+            int directoryCount = Directory.GetDirectories(folderPath).Length;
+
+            string relativePath = folderPath.Substring(rootFolderPath.Length).TrimStart('\\');
+
+            folderInfo[relativePath] = (folderSize, fileCount, directoryCount);
+
+            foreach (string subdirectory in Directory.GetDirectories(folderPath))
+            {
+                TraverseFolder(subdirectory, folderInfo, rootFolderPath);
+            }
+        }
+
+        static long GetDirectorySize(DirectoryInfo directoryInfo)
+        {
+            long size = 0;
+
+            // Add file sizes.
+            FileInfo[] files = directoryInfo.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                size += file.Length;
+            }
+
+            // Add subdirectory sizes.
+            DirectoryInfo[] directories = directoryInfo.GetDirectories();
+            foreach (DirectoryInfo directory in directories)
+            {
+                size += GetDirectorySize(directory);
+            }
+
+            return size;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        //    // Collect information from the first folder
+        //    Dictionary<string, (long Size, int FileCount, int DirectoryCount, string s, long Size1, int Filecount1, int DirectoryCount1)> firstFolderInfo = new Dictionary<string, (long, int, int, string, long, int, int)>();
+        //    TraverseFolder(firstFolderPath, firstFolderInfo, firstFolderPath);
+
+        //        using (StreamWriter writer = new StreamWriter(csvFilePath))
+        //        {
+        //            writer.WriteLine("RelativePath,FirstFolderSize,FirstFileCount,FirstDirectoryCount,SecondFolderSize,SecondFileCount,SecondDirectoryCount");
+
+        //            // Compare with the second folder
+        //            foreach (var kvp in firstFolderInfo)
+        //            {
+        //                string relativePath = kvp.Key; // Already stored as relative path
+        //string secondFolderPathEquivalent = Path.Combine(secondFolderPath, relativePath);
+
+        //                if (Directory.Exists(secondFolderPathEquivalent))
+        //                {
+        //                    long secondFolderSize = GetDirectorySize(new DirectoryInfo(secondFolderPathEquivalent));
+        //int secondFileCount = Directory.GetFiles(secondFolderPathEquivalent).Length;
+        //int secondDirectoryCount = Directory.GetDirectories(secondFolderPathEquivalent).Length;
+
+        //writer.WriteLine($"{relativePath},{kvp.Value.Size},{kvp.Value.FileCount},{kvp.Value.DirectoryCount},{secondFolderSize},{secondFileCount},{secondDirectoryCount}");
+        //                }
+        //                else
+        //                {
+        //                    writer.WriteLine($"{relativePath},{kvp.Value.Size},{kvp.Value.FileCount},{kvp.Value.DirectoryCount},0,0,0");
+        //                }
+        //            }
+        //        }
+
+        //        Console.WriteLine("CSV file created successfully.");
+
+        //static void TraverseFolder(string folderPath, Dictionary<string, (long, int, int, string,  long, int, int)> folderInfo, string rootFolderPath)
+        //{
+        //    long folderSize = GetDirectorySize(new DirectoryInfo(folderPath));
+        //    int fileCount = Directory.GetFiles(folderPath).Length;
+        //    int directoryCount = Directory.GetDirectories(folderPath).Length;
+
+        //    string transformed = folderPath.Replace(@"D:\", "");
+
+        //    long folderSize1 = GetDirectorySize(new DirectoryInfo(folderPath));
+        //    int fileCount1 = Directory.GetFiles(folderPath).Length;
+        //    int directoryCount1 = Directory.GetDirectories(folderPath).Length;
+
+        //    string relativePath = folderPath.Substring(rootFolderPath.Length).TrimStart('\\');
+
+        //    folderInfo[relativePath] = (folderSize, fileCount, directoryCount, " ",folderSize1, fileCount1 ,directoryCount1 );
+
+        //    foreach (string subdirectory in Directory.GetDirectories(folderPath))
+        //    {
+        //        TraverseFolder(subdirectory, folderInfo, rootFolderPath);
+        //    }
+        //}
+
+        //static long GetDirectorySize(DirectoryInfo directoryInfo)
+        //{
+        //    long size = 0;
+
+        //    // Add file sizes.
+        //    FileInfo[] files = directoryInfo.GetFiles();
+        //    foreach (FileInfo file in files)
+        //    {
+        //        size += file.Length;
+        //    }
+
+        //    // Add subdirectory sizes.
+        //    DirectoryInfo[] directories = directoryInfo.GetDirectories();
+        //    foreach (DirectoryInfo directory in directories)
+        //    {
+        //        size += GetDirectorySize(directory);
+        //    }
+
+        //    return size;
+        //}
+
+    }
+    }
